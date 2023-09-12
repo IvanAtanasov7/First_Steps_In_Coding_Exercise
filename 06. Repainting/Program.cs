deba9f7d11a57553e868696f4bab94101807cbe9
Ivan Atanasov <129539509+IvanAtanasov7@users.noreply.github.com>
2023-09-12 20:43:17 +0300

    Update Program.cs

--- 06. Repainting/Program.cs
+++ 06. Repainting/Program.cs
@@ -10,11 +10,12 @@ namespace _06._Repainting
 			int paint = int.Parse(Console.ReadLine());
 			int paintThinner = int.Parse(Console.ReadLine());
 			int hours = int.Parse(Console.ReadLine());
+			double bagPrice = 0.40;
 
 			double nylonMoney = (nylon + 2) * 1.50;
 			double paintMoney = (paint * 1.1) * 14.50;
 			int paintThinnerMoney = paintThinner * 5;
-			double materialsMoney = nylonMoney + paintMoney + paintThinnerMoney + 0.40;
+			double materialsMoney = nylonMoney + paintMoney + paintThinnerMoney + bagPrice;
 			double workersPay = (materialsMoney * 0.3) * hours;
 			double totalSum = materialsMoney + workersPay;
 
